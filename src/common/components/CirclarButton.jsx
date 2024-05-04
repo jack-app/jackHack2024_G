050c3ca4bd522ca2736c5057a139068fc6b58597
hirakuuuu <m.hiraku1129@gmail.com>
2024-05-04 17:32:15 +0900

    [add] Topページのひな型

---
+++ src/common/components/CirclarButton.jsx
@@ -0,0 +1,34 @@
+import PropTypes from "prop-types";
+
+const CirclarButton = (props) => {
+  // propsを分割代入
+  const { color, icon } = props;
+  const circlarButtonStyle = {
+    border: `4px solid ${color}`,
+    backgroundColor: "#FFFFFF",
+    height: "70px",
+    borderRadius: "50%",
+    width: "70px",
+    boxShadow: "0 0 10px rgba(0, 0, 0, 0.2)",
+    alignItems: "center",
+    display: "flex",
+    justifyContent: "center",
+    color: `${color}`,
+    transition: "0.2s cubic-bezier(0.45, 0, 0.55, 1)",
+  };
+
+  return (
+    <>
+      <div style={circlarButtonStyle} className="fuwatto">
+        <i className={`fas fa-${icon} fa-2x`}></i>
+      </div>
+    </>
+  );
+};
+
+CirclarButton.propTypes = {
+  color: PropTypes.string,
+  icon: PropTypes.string,
+};
+
+export default CirclarButton;
--- src/features/Top/components/Top.jsx
+++ src/features/Top/components/Top.jsx
@@ -1,10 +1,30 @@
-import React from "react";
+import Map from "./Map";
 import "../styles/Top.css";
+import CirclarButton from "@common/components/CirclarButton";
 
 const Top = () => {
   return (
     <>
-      <h1 className="heading">LOST & FOUND</h1>
+      <div>
+        <h1 className="heading">LOST & FOUND</h1>
+      </div>
+      <div className="search-form-container field">
+        <div className="field">
+          <div className="control">
+            <input className="input" type="text" placeholder="e.g smartphone" />
+          </div>
+        </div>
+      </div>
+      <div className="columns">
+        <div className="column is-10">
+          <Map />
+        </div>
+        <div className="column is-2 button-container" style={{ margin: "1em" }}>
+          <CirclarButton color="#FFE127" icon="star" />
+          <CirclarButton color="#7181FF" icon="question" />
+          <CirclarButton color="#6FF75B" icon="plus" />
+        </div>
+      </div>
     </>
   );
 };
