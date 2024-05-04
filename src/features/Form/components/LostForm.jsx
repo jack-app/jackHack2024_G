7a6834493f47f8ff7f3cdddd0baaf5a139dc3669
ASANUMA-Katsuya <kattun.python@gmail.com>
2024-05-04 14:08:26 +0900

    [add]文字だけ

--- src/features/Form/components/LostForm.jsx
+++ src/features/Form/components/LostForm.jsx
@@ -3,9 +3,16 @@ import '../styles/LostForm.css'
 
 const LostForm = () => {
   return (
-    <div className='red'>
-      hello,world
-    </div>
+   <>
+   <div>
+    <h1>落とし物追加フォーム</h1>
+    <h3>LOST PROPERTY</h3>
+    <h3>PLACE</h3>
+    <h3>TIME</h3>
+    <h3>COLOR</h3>
+    <h3>DETAIL</h3>
+   </div>
+   </>
   )
 }
 
