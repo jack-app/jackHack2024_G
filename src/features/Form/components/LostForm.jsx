2b956d7e347a0ccf5b44f42fce60b962b14daec7
ASANUMA-Katsuya <kattun.python@gmail.com>
2024-05-04 13:51:07 +0900

    [add]lostformのファイル

---
+++ src/features/Form/components/LostForm.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../styles/LostForm.css'
+
+const LostForm = () => {
+  return (
+    <div className='red'>
+      hello,world
+    </div>
+  )
+}
+
+export default LostForm
