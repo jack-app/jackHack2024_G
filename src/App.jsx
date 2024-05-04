2ac7e72ceb41e884af7c4f0da548a5924ca5a1ca
hirakuuuu <m.hiraku1129@gmail.com>
2024-05-04 20:58:35 +0900

    [add] Layout.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
-import "./App.css";
+import "@common/styles/Layout.css";
 import Top from "@features/Top/components/Top";
 
 function App() {
