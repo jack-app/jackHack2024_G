171a3f522421e1e73ac397ef945536aef8e1dddf
hirakuuuu <m.hiraku1129@gmail.com>
2024-05-04 14:14:08 +0900

    [add] Topページのファイル

---
+++ src/features/Top/components/Top.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "../styles/Top.css";
+
+const Top = () => {
+  return (
+    <>
+      <h1 className="heading">LOST & FOUND</h1>
+    </>
+  );
+};
+
+export default Top;
