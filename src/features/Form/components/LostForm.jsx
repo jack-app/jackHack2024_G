8a905be7e2cdab274e6c3671b95202ede4015a9b
ASANUMA-Katsuya <kattun.python@gmail.com>
2024-05-04 15:28:51 +0900

    [add]分割

--- src/features/Form/components/LostForm.jsx
+++ src/features/Form/components/LostForm.jsx
@@ -4,9 +4,24 @@ import '../styles/LostForm.css'
 const LostForm = () => {
   return (
    <>
-    <div class = "content is-medium">
-      
+    <div class = "container">
+      <h1 class = "title">
+        落とし物追加フォーム
+      </h1>
+      <div class = "columns">
+        <div class = "column is-6">
+          {/*後ほど写真フォーム追加*/}
+          <h2>左に表示したい</h2>       
+        </div>
+        <div class = "column is-6">
+          <h2>
+            右に表示したい
+          </h2>
+        </div>    
+      </div>
+
     </div>
+   
    </>
   )
 }
