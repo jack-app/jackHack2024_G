a83e26f0d458800d6c36341ff26007d75f062fdf
ASANUMA-Katsuya <kattun.python@gmail.com>
2024-05-04 14:46:57 +0900

    [add]BULMA使うのでいったん削除

--- src/features/Form/components/LostForm.jsx
+++ src/features/Form/components/LostForm.jsx
@@ -4,14 +4,9 @@ import '../styles/LostForm.css'
 const LostForm = () => {
   return (
    <>
-   <div>
-    <h1>落とし物追加フォーム</h1>
-    <h3>LOST PROPERTY</h3>
-    <h3>PLACE</h3>
-    <h3>TIME</h3>
-    <h3>COLOR</h3>
-    <h3>DETAIL</h3>
-   </div>
+    <div class = "content is-medium">
+      
+    </div>
    </>
   )
 }
